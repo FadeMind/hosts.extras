ccceb63a7d7a1481a981b1d254d92b5bb281b939
fademind <fademind@gmail.com>
2018-02-10 11:23:16 +0100

    00checksum.sha512.gen.bash fix

--- 00checksum.sha512.gen.bash
+++ 00checksum.sha512.gen.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 if [ -f CHECKSUMS.SHA512 ] ; then rm CHECKSUMS.SHA512 ; fi
-sha512sum add.*/* hpHosts*/* rlwpx.free.fr.h*/* *Ads/* > CHECKSUMS.SHA512
+sha512sum add.*/* hpHosts*/* rlwpx.free.fr.h*/* *Ads/* Spotify-Ad-free/* > CHECKSUMS.SHA512
