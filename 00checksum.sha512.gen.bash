b2a1c8cba31936d91966d9c33f325f799b8c751e
fademind <fademind@gmail.com>
2018-02-12 09:45:34 +0100

    00checksum.sha512.gen.bash: add tree

--- 00checksum.sha512.gen.bash
+++ 00checksum.sha512.gen.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 if [ -f CHECKSUMS.SHA512 ] ; then rm CHECKSUMS.SHA512 ; fi
 sha512sum add.*/* hpHosts*/* rlwpx.free.fr.h*/* Spotify-Ad-free/* *Ads/* > CHECKSUMS.SHA512
+if [ -f TREE ] ; then rm TREE ; fi
+tree > TREE
